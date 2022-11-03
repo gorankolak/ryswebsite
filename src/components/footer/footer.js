b1ab6eb77cdaee4acbd9503774d4d25fa90e4018
Goran Kolak <goran.kolak82@gmail.com>
2022-11-03 22:58:52 +0100

    cookies consent popup disabled

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -7,14 +7,14 @@ import styles from "./footer.module.css"
 const Footer = () => {
   return (
     <footer id="contact" class="footer" className={styles.footer}>
-      <CookieConsent
+      {/* <CookieConsent
         location="bottom"
         buttonText="Accept"
         declineButtonText="Decline"
         cookieName="gatsby-gdpr-google-analytics"
       >
         This site uses cookies to offer you a better browsing experience.
-      </CookieConsent>
+      </CookieConsent> */}
       {/* <SVGfooter1 class="svgfooter1 svgs" /> */}
       <div className="footer-left">
         <p class="footer-subtitle">contact</p>
